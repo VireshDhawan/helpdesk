70d8ba81acbd7c50bb9286a3393143434b1b67ce
Mrudul T <mrudultarwatkar@gmail.com>
2014-03-28 18:47:44 +0530

    initial bitbucket commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helpdesk::Application.config.secret_key_base = '8248d043f920a26de690c25b02a8215d75282c25d7b17ba9acf728f213fa8c43db7c86b39d1a61a79ce5dcc5c5cfac747355be0f51f4408fa4b85c498f387c55'
